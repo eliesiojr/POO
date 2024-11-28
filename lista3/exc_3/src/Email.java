c3a7ed04d075687706d534f9123cee4a1cfc2433
eliesiojr <eliesio.xavierjr@hotmail.com>
2024-11-28 20:57:27 -0300

    resolução exc 3 lista 3

--- lista3/exc_3/src/Email.java
+++
@@ -1,6 +0,0 @@
-
-public class Email implements Notificacao{
-    public void enviarMensagem() {
-        System.out.println("Enviando mensagem via email");
-    }
-}
--- lista3/exc_3/src/PushN.java
+++
@@ -1,5 +0,0 @@
-public class PushN implements Notificacao {
-    public void enviarMensagem() {
-        System.out.println("Enviando mensagem via push");
-    }
-}
